19306742e2e303464a40bf8618e12a2863cac81e
loubna213 <loub60418@gmail.com>
2024-12-14 15:31:55 +0100

    add assets to public folder and edit the layouts with next/fonts and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,27 @@
 import { ClerkProvider, UserButton } from '@clerk/nextjs'
 import './globals.css'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "Acommunity- driven platform for asking and answering programming question. Get help, share knowledge, and collaborate with developers form around the world.",
+  icons: {
+    icon: "/assets/images/site-logo.svg"
+  }
+}
 
 export default function RootLayout({
   children,
@@ -7,9 +29,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimarey: 'primary-gradiant',
+        footerActionLink: 'primary-text-gradiant hover:text-primary-500',
+      }
+    }}>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <UserButton />
           {children}
         </body>
